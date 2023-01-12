e108851fea0b93c6fe56b29450d5211d39c84e93
AliCMA <32778@ma-web.nl>
2023-01-12 13:03:41 +0100

    webshop prog

---
+++ home.php
@@ -0,0 +1,80 @@
+<?php 
+   session_start();
+   include "db_conn.php";
+   if (isset($_SESSION['username']) && isset($_SESSION['id'])) {   ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>HOME</title>
+	<link rel="stylesheet" href="indexphp.css">
+</head>
+<body>
+      <div class="container d-flex justify-content-center align-items-center"
+      style="min-height: 100vh">
+      	<?php if ($_SESSION['role'] == 'admin') {?>
+      	
+      		<div class="card" style="width: 18rem;">
+			  <img src="img/admin-default.png" 
+			       class="card-img-top" 
+			       alt="admin image">
+			  <div class="card-body text-center">
+			    <h5 class="card-title">
+			    	<?=$_SESSION['name']?>
+			    </h5>
+
+			    <a href="logout.php" class="btn btn-dark">Logout</a>
+			  </div>
+			</div>
+			<div class="p-3">
+				<?php include 'php/members.php';
+                 if (mysqli_num_rows($res) > 0) {?>
+                  
+				<h1 class="display-4 fs-1">Members</h1>
+				<table class="table" 
+				       style="width: 32rem;">
+				  <thead>
+				    <tr>
+				      <th scope="col">#</th>
+				      <th scope="col">Name</th>
+				      <th scope="col">User name</th>
+				      <th scope="col">Role</th>
+				    </tr>
+				  </thead>
+				  <tbody>
+				  	<?php 
+				  	$i =1;
+				  	while ($rows = mysqli_fetch_assoc($res)) {?>
+				    <tr>
+				      <th scope="row"><?=$i?></th>
+				      <td><?=$rows['name']?></td>
+				      <td><?=$rows['username']?></td>
+				      <td><?=$rows['role']?></td>
+				    </tr>
+				    <?php $i++; }?>
+				  </tbody>
+				</table>
+				<?php }?>
+			</div>
+      	<?php }else { ?>
+      	
+      		<div class="card" style="width: 18rem;">
+			  <img src="img/user-default.png" 
+			       class="card-img-top" 
+			       alt="admin image">
+			  <div class="card-body text-center">
+			    <h5 class="card-title">
+			    	<?=$_SESSION['name']?>
+			    </h5>
+			    <a href="logout.php" class="btn btn-dark">Logout</a>
+			  </div>
+			</div>
+      	<?php } ?>
+      </div>
+</body>
+</html>
+<?php }else{
+	header("Location: index.php");
+} ?>
+
+	
